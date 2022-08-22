2d113d4e4d894734472e228b806cc42ed056acb7
David Almeida <djalmeida3@gmail.com>
2022-08-22 16:03:53 -0400

    Final changes

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,22 +1,79 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useReducer, useMemo, useRef, useCallback } from 'react';
+import useCharacters from '../hooks/useCharacters';
+import Search from './Search';
+
+
+const initialState = {
+  favorites: []
+}
+
+const API = 'https://rickandmortyapi.com/api/character'
+
+const favoriteReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TO_FAVORITE':
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload]
+      };
+    default:
+      return state;
+  }
+}
 
 const Characters = () => {
-  const [characters, setCharacters] = useState([]);
+  const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+  const [search, setSearch] = useState('');
+  const searchInput = useRef(null);
 
-  useEffect(() => {
-    fetch('https://rickandmortyapi.com/api/character')
-      .then(response => response.json())
-      .then(data => setCharacters(data.results));
+  const characters = useCharacters(API);
+
+  const handleSearch = useCallback(() => {
+    setSearch(searchInput.current.value);
   }, []);
+
+  // Working without callback
+  // const handleSearch = () => {
+  //   setSearch(searchInput.current.value);
+  // }
+
+
+  const handleClick = favorite => {
+    dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
+  }
+
+  // Working without UseMemo
+  // const filteredUsers = characters.filter((user) => {
+  //   return user.name.toLowerCase().includes(search.toLowerCase());
+  // })
+
+  const filteredUsers = useMemo(() =>
+    characters.filter((user) => {
+      return user.name.toLowerCase().includes(search.toLowerCase());
+    }),
+      [characters, search]
+  )
 
   return (
     <div className="container">
       <div className="row">
-        {characters.map((character) => (
-          <div className="col">
+        
+        {favorites.favorites.map(favorite =>(
+          <li key={favorite.id}>
+            {favorite.name}
+          </li>
+        ))}
+
+        <Search search={search} searchInput={searchInput} handleSearch={handleSearch} />
+
+        {filteredUsers.map((character) => (
+          <div className="col item" key={character.id}>
             <>
               <img className="character__img" src={character.image} alt="" />
               <h2 className="character__name">{character.name}</h2>
+              <button type='button' onClick={() => handleClick(character)}>
+                Agregar a favoritos
+              </button>
             </>
           </div>
         ))}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import ThemeContext from "../context/ThemeContext";
 
 export const Header = (props) => {
+  const color = useContext(ThemeContext);
   return (
-    <div className="Header">
+    <div style={{ color }} className="Header">
       <h1>ReactHooks</h1>
       <button type="button" onClick={() => props.onClick()}>
         {props.darkMode ? 'Dark Mode' : 'Light Mode'}
